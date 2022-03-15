54f16d376acc3e938709e36dcb8855aa3e19bb03
AlxandrV <a.vigneaud@gmail.com>
2022-03-15 17:44:50 +0100

    Add - add LoginView and update settings

--- LITReview/LITReview/urls.py
+++ LITReview/LITReview/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LoginView.as_view(
+            template_name='authentication/login.html',
+            redirect_authenticated_user=True),
+         name='login')
 ]
