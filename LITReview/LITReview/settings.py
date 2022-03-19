4e5fc3bceaf36fdeb75b408fdf1f8cc1b4c6a35c
AlxandrV <a.vigneaud@gmail.com>
2022-03-19 18:40:09 +0100

    Update - Revoke secret key

--- LITReview/LITReview/settings.py
+++ LITReview/LITReview/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=#9l)+0uo2c+co(p4lzoe+z$mah!h$k#at_grp-mp79=wul3p)'
-
+SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
