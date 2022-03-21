92c582100762a525e0350eae53ba49aeeffc2c5d
AlxandrV <a.vigneaud@gmail.com>
2022-03-21 19:07:20 +0100

    Update - change label ticket form

--- LITReview/review/forms.py
+++ LITReview/review/forms.py
@@ -1,3 +1,4 @@
+from cProfile import label
 from django import forms
 
 from review.models import Ticket
@@ -5,4 +6,7 @@ from review.models import Ticket
 class TicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
+        labels = {
+            'title': 'Titre',
+        }
         exclude = ('user', 'time_created')
